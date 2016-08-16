fb7e65699c4d1e02affed539c9549942efb71c6b
Studio <studio@bazzoo.co.uk>
2016-08-16 13:52:44 +0100

    Removed Random Brackets

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
 
                             if ($aup) {
                               echo "<i class='fa fa-check status-up' aria-hidden='true'></i><br>";
-                              echo "<h3 class='status-box'>American Server is Responding.<br>()</h3>";
+                              echo "<h3 class='status-box'>American Server is Responding.</h3>";
                             }
 
                             else {

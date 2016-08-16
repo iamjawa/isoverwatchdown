3116020cae21a7890ae503f9bf9c959769935748
Billy Burrows <hello@iamjawa.co>
2016-08-16 20:39:15 +0000

    Links

--- index.php
+++ index.php
@@ -29,13 +29,13 @@
                 return $rval === 0;
         }
 
-        $eu = '185.60.112.157';
+        $eu = 'overwatchtracker.com';
         $eup = ping($eu);
         
-        $american = '37.244.0.3';
+        $american = 'overwatchtracker.com';
         $aup = ping($american);
         
-        $korean = '121.254.206.1';
+        $korean = 'overwatchtracker.com';
         $kup = ping($korean);
 
         ?>

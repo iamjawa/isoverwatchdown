29228e7a2c46e1db4e73bbdbf3366b347cd9257a
Studio <studio@bazzoo.co.uk>
2016-08-16 09:24:37 +0100

    Initial Files and Content

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="17, Freelance Web Developer, UK.">
+        <meta name="keywords" content="Genuinity, Genuine">
+        <meta name="authxor" content="iamjawa">
+        <meta name="theme-color" content="#435662">
+        <title>Is Overwatch Down?</title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="/css/main.css" type="text/css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/materialize/0.97.7/css/materialize.min.css">
+        <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+    </head>
+
+    <body>
+      <main>
+        <h1 class="main-header">Is Overwatch <span class="down-title">Down?</span></h1>
+      </main>
+    </body>
+    
+</html>

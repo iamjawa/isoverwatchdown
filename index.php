cfe951aedfa73526e9ab64ad471a3760371393dc
Billy <hello@iamjawa.co>
2016-08-18 12:19:00 +0100

    Moved Ping to External File

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 
-function ping($host)
-{
-        exec(sprintf('ping -c 1 -W 2 %s', escapeshellarg($host)), $res, $rval);
-        return $rval === 0;
-}
-
-$eu = 'overwatchtracker.com';
-$eup = ping($eu);
-
-$american = 'overwatchtracker.com';
-$aup = ping($american);
-
-$korean = 'overwatchtracker.com';
-$kup = ping($korean);
+include "ping.php";
 
 ?>
 
